88dd817c680ba7bbc174a8d9fccee8ab20b2b72d
Jan Zisenis <jan.zisenis@gmail.com>
2024-02-09 18:17:07 +0100

    change the formating in the flake.nix

--- flake.nix
+++ flake.nix
@@ -79,8 +79,8 @@
               inherit (inputs.nix-colors.lib-contrib {inherit pkgs;}) gtkThemeFromScheme;
             };
 	        home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "backup";
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
 	        home-manager.users.${username} = import ./home.nix;
 	      }
 	    ];
